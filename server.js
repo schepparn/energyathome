5409e742f6f5e615e439a9dad26c2a5c322ee56d
Anders Dalen <andersdalen@gmail.com>
2014-01-21 21:34:08 +0100

    changed ownership and config files

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 
 /*!
+ * energyathome
+ * Copyright(c) 2013 Anders Dalen <andersdalen@gmail.com>
+ * MIT Licensed
+ *
+ * Boilerplate and Passport implementation based on:
  * nodejs-express-mongoose-demo
  * Copyright(c) 2013 Madhusudhan Srinivasa <madhums8@gmail.com>
  * MIT Licensed
